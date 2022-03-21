be193b60e706798b94a87b5c1b1ebee18fa533c2
Polarnighty <1041465280@qq.com>
2022-03-21 22:38:08 +0800

    修复了贪吃蛇的一些BUG

--- Snake/Model/SnakeEye.cs
+++ Snake/Model/SnakeEye.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Snake.Model
 {
-    public class SnakeEye
+    public class SnakeEye: SnakePart
     {
+        public SnakeEye(double gameBoardWidthPixels, double gameBoardHeightPixels, double initialXPosition, double initialYPosition, Direction direction)
+    : base(gameBoardWidthPixels, gameBoardHeightPixels, direction)
+        {
+            _xPosition = initialXPosition;
+            _yPosition = initialYPosition - Constants.EyeOffet;
+            _width = Constants.EyeWidth;
+            _height = Constants.EyeHeight;
+        }
+
     }
+
 }
--- Snake/Model/SnakePart.cs
+++ Snake/Model/SnakePart.cs
@@ -2,6 +2,74 @@
 {
     public abstract class SnakePart: GameBoardItem
     {
+        protected Direction _directionOfTravel;
+
+        public SnakePart(double gameBoardWidthPixels, double gameBoardHeightPixels, Direction direction)
+         : base(gameBoardWidthPixels, gameBoardHeightPixels)
+        {
+            _directionOfTravel = direction;
+        }
+
+        public Direction DirectionOfTravel
+        {
+            get
+            {
+                return _directionOfTravel;
+            }
+            set
+            {
+                _directionOfTravel = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(DirectionOfTravelDegrees));
+            }
+        }
+
+        public double DirectionOfTravelDegrees
+        {
+            get
+            {
+                double direction = 0;
+                switch (_directionOfTravel)
+                {
+                    case Direction.Up:
+                        direction = Constants.DirectionUpDegrees;
+                        break;
+                    case Direction.Down:
+                        direction = Constants.DirectionDownDegrees;
+                        break;
+                    case Direction.Left:
+                        direction = Constants.DirectionLeftDegrees;
+                        break;
+                    case Direction.Right:
+                        direction = Constants.DirectionRightDegrees;
+                        break;
+                    default:
+                        break;
+                }               
+                return direction;
+            }
+        }
+
+        public void UpdatePosition()
+        {
+            switch (_directionOfTravel)
+            {
+                case Direction.Up:
+                    YPosition = YPosition - Constants.StepSize;
+                    break;
+                case Direction.Down:
+                    YPosition = YPosition + Constants.StepSize;
+                    break;
+                case Direction.Left:
+                    XPosition = XPosition - Constants.StepSize;
+                    break;
+                case Direction.Right:
+                    XPosition = XPosition + Constants.StepSize;
+                    break;
+                default:
+                    break;
+            }  
+        }
 
     }
 }
